bedc68fd0ffe08b326a83db063735a56f59a8174
flygohr <info@flygohr.com>
2026-05-15 21:02:44 +0200

    zoom fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('../images/mapv2/{z}/{x}/{y}.png', {
+L.tileLayer('../images/mapv3/{z}/{x}/{y}.png', {
     minZoom: 1,
-    maxZoom: 8,
+    maxZoom: 1,
     attribution: '&copy; <a href="https://nuradan.flygohr.com">Nuradan Project</a>'
 }).addTo(map);
