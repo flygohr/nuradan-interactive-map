9835d285adba8fa80e29bf47d88be2470a41570b
flygohr <info@flygohr.com>
2026-05-15 20:54:50 +0200

    testing tiling

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('../images/map.png', {
-    maxZoom: 19,
+L.tileLayer('../images/mapv1/{z}/{x}/{y}.png', {
+    minZoom: 4,
+    maxZoom: 4,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
