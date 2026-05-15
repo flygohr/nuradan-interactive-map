5e4c6f4b38d9515e8571450b28b9c84c91184068
flygohr <info@flygohr.com>
2026-05-15 21:05:28 +0200

    idk anymore

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('../images/mapv3/{z}/{x}/{y}.png', {
-    minZoom: 1,
-    maxZoom: 1,
+L.tileLayer('../images/mapv1/{z}/{x}/{y}.png', {
+    maxZoom: 4,
     attribution: '&copy; <a href="https://nuradan.flygohr.com">Nuradan Project</a>'
 }).addTo(map);
