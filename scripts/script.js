a49130b1c455ad3507477e3b550dbdd288872efc
flygohr <info@flygohr.com>
2026-05-16 13:34:07 +0200

    trying a new approach

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-var map = L.map('map').setView([50, 10], 6);
-
-L.tileLayer('../images/mapv2/{z}/{x}/{y}.png', {
-    minZoom: 6,
-    maxZoom: 6,
-    attribution: '&copy; <a href="https://nuradan.flygohr.com">Nuradan Project</a>'
-}).addTo(map);
