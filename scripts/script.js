0b0e4206b926bd75260d2e7effaa6d5ef246401b
flygohr <info@flygohr.com>
2026-05-16 10:16:10 +0200

    testing different zoom levels

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('../images/mapv1/{z}/{x}/{y}.png', {
-    minZoom: 6,
-    maxZoom: 6,
+L.tileLayer('../images/mapv2/{z}/{x}/{y}.png', {
+    minZoom: 5,
+    maxZoom: 7,
     attribution: '&copy; <a href="https://nuradan.flygohr.com">Nuradan Project</a>'
 }).addTo(map);
